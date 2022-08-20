571f0ca179a951271e19f3ee94a2adbaabb2bc1c
Lim En Xi <limenxi07@gmail.com>
2022-08-20 10:21:45 +0800

    quote button draft

--- 3-front-end-development/random-quote-machine/src/App.js
+++ 3-front-end-development/random-quote-machine/src/App.js
@@ -10,7 +10,9 @@ function App() {
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
 
-  useEffect(() => {
+  const handleClick = () => fetchQuote();
+
+  const fetchQuote = useEffect(() => {
     let rand = Math.floor(Math.random() * 100);
     fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json').then(res => res.json()).then((data) => {
       setQuote(data['quotes'][rand]['quote']);
@@ -23,7 +25,7 @@ function App() {
       <div id="quote-container">
         <p id="quote">{quote}</p>
         <p id="author">{author}</p>
-        <Button id="new-quote">New quote</Button>
+        <button id="new-quote" onClick={handleClick}>New quote</button>
         <Button id="tweet-quote">Tweet</Button>
       </div>
     </div>
