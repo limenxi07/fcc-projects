360c6a949d157078d28ad9002dbba169bf0c7b52
Lim En Xi <limenxi07@gmail.com>
2022-08-20 09:44:48 +0800

    layout

--- 3-front-end-development/random-quote-machine/src/App.js
+++ 3-front-end-development/random-quote-machine/src/App.js
@@ -1,9 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <div id="quote-container">
+        <p id="quote">Quote</p>
+        <p id="author">Author</p>
+        <Button id="new-quote">New quote</Button>
+        <Button id="tweet-quote">Tweet</Button>
+      </div>
     </div>
   );
 }
