be06176d8604b4397caa5201bc68ac765dba65a0
Lim En Xi <limenxi07@gmail.com>
2022-08-21 11:32:07 +0800

    newquote button

--- 3-front-end-development/random-quote-machine/src/App.js
+++ 3-front-end-development/random-quote-machine/src/App.js
@@ -10,22 +10,24 @@ function App() {
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
 
-  const handleClick = () => fetchQuote();
+  useEffect(() => fetchQuote(), []);
 
-  const fetchQuote = useEffect(() => {
+  const fetchQuote = () => {
     let rand = Math.floor(Math.random() * 100);
-    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json').then(res => res.json()).then((data) => {
+    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    .then(res => res.json())
+    .then((data) => {
       setQuote(data['quotes'][rand]['quote']);
       setAuthor(data['quotes'][rand]['author']);
     });
-  }, []);
+  };
 
   return (
     <div className="App">
       <div id="quote-container">
         <p id="quote">{quote}</p>
         <p id="author">{author}</p>
-        <button id="new-quote" onClick={handleClick}>New quote</button>
+        <button id="new-quote" onClick={fetchQuote}>New quote</button>
         <Button id="tweet-quote">Tweet</Button>
       </div>
     </div>
