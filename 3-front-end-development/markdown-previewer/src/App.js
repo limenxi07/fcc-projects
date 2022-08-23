de96200acf61d02fb9fa3f6ed5776a305bdec89a
Lim En Xi <limenxi07@gmail.com>
2022-08-23 20:24:41 +0800

    editor draft

--- 3-front-end-development/markdown-previewer/src/App.js
+++ 3-front-end-development/markdown-previewer/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { marked } from 'marked';
+
 import './App.scss';
 
 const App = () => {
+  const [markdown, setMarkdown] = useState('PLACEHOLDER');
+
+  const onContentChange = (event) => {
+    setMarkdown(marked.parse(event.target.value));
+  }
+  
   return (
     <div className="App">
-      
+      <textarea id="editor" name="editor" onChange={onContentChange}></textarea>
+      <div id="preview"></div>
     </div>
   );
 }
