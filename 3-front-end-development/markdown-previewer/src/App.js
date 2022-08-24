1f38f6fd6fb251b5402cf0cd25771a2f6ccfccc0
Lim En Xi <limenxi07@gmail.com>
2022-08-24 19:12:55 +0800

    placeholder text

--- 3-front-end-development/markdown-previewer/src/App.js
+++ 3-front-end-development/markdown-previewer/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { marked } from 'marked';
+import Prism from 'prismjs';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,7 +8,28 @@ import Col from 'react-bootstrap/Col';
 import './App.scss';
 
 const App = () => {
-  const [markdown, setMarkdown] = useState('PLACEHOLDER');
+  marked.setOptions({ // enable line breaks with return button
+    breaks: true,
+    highlight: function (code) {
+      return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+    }
+  });
+
+  const [markdown, setMarkdown] = useState(`
+  # heading
+  ## subheading
+  [link](https://github.com/)
+  inline code \` var x = 4 \`
+  - list item 
+  \`\`\` 
+  multiline 
+  code 
+  \`\`\`
+  **bold text**
+  _italics_
+  > block quote
+  ![google logo](https://img.icons8.com/color/48/000000/google-logo.png)
+  `); // placeholder text
 
   const onContentChange = (event) => {
     setMarkdown(event.target.value);
@@ -22,7 +44,7 @@ const App = () => {
         <Row>
           <Col>
             <h2>Editor</h2>
-            <textarea id="editor" name="editor" onChange={onContentChange}></textarea>
+            <textarea id="editor" name="editor" onChange={onContentChange} value={markdown}></textarea>
           </Col>
           <Col>
             <h2>Preview</h2>
