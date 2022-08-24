42d9cc4fbcec4a20f5a32bf533f936205bbd096d
Lim En Xi <limenxi07@gmail.com>
2022-08-24 18:26:27 +0800

    markdown parser

--- 3-front-end-development/markdown-previewer/src/App.js
+++ 3-front-end-development/markdown-previewer/src/App.js
@@ -7,13 +7,15 @@ const App = () => {
   const [markdown, setMarkdown] = useState('PLACEHOLDER');
 
   const onContentChange = (event) => {
-    setMarkdown(marked.parse(event.target.value));
+    setMarkdown(event.target.value);
   }
   
   return (
     <div className="App">
       <textarea id="editor" name="editor" onChange={onContentChange}></textarea>
-      <div id="preview"></div>
+      <div id="preview" dangerouslySetInnerHTML={{
+        __html: marked(markdown)
+      }}></div>
     </div>
   );
 }
