ecd24944e39a51b8b67e3780a0e0ff1e21893a90
Lim En Xi <limenxi07@gmail.com>
2022-08-27 10:44:53 +0800

    html content

--- 3-front-end-development/javascript-calculator/src/App.js
+++ 3-front-end-development/javascript-calculator/src/App.js
@@ -7,15 +7,35 @@ import './App.scss';
 const App = () => {
   return (
     <div className="App">
-      <Container id="display">
+      <Container id="main-container">
+        <p id="display">0</p>
         <Row>
-          <Col xs={6}>1</Col>
-          <Col>2</Col>
+          <Col className="button ac" id="clear" xs={6}>AC</Col>
+          <Col className="button operator" id="change-sign">+/-</Col>
+          <Col className="button operator" id="divide">÷</Col>
         </Row>
         <Row>
-          <Col>1</Col>
-          <Col xs={3}>2</Col>
-          <Col xs={3}>3</Col>
+          <Col className="button" id="seven">7</Col>
+          <Col className="button" id="eight">8</Col>
+          <Col className="button" id="nine">9</Col>
+          <Col className="button operator" id="multiply">×</Col>
+        </Row>
+        <Row>
+          <Col className="button" id="four">4</Col>
+          <Col className="button" id="five">5</Col>
+          <Col className="button" id="six">6</Col>
+          <Col className="button operator" id="subtract">-</Col>
+        </Row>
+        <Row>
+          <Col className="button" id="one">1</Col>
+          <Col className="button" id="two">2</Col>
+          <Col className="button" id="three">3</Col>
+          <Col className="button operator" id="add">+</Col>
+        </Row>
+        <Row>
+          <Col className="button" id="zero" xs={6}>0</Col>
+          <Col className="button" id="decimal">.</Col>
+          <Col className="button eq" id="equals">=</Col>
         </Row>
       </Container>
     </div>
