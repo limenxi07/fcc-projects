d3d2c0d0fce850bb78e78fe060894457d3d2d7d0
Lim En Xi <limenxi07@gmail.com>
2022-08-27 10:14:31 +0800

    bootstrap layout draft

--- 3-front-end-development/javascript-calculator/src/App.js
+++ 3-front-end-development/javascript-calculator/src/App.js
@@ -1,10 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { useState } from 'react';
 import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
-
+      <Container id="display">
+        <Row>
+          <Col xs={6}>1</Col>
+          <Col>2</Col>
+        </Row>
+        <Row>
+          <Col>1</Col>
+          <Col xs={3}>2</Col>
+          <Col xs={3}>3</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
