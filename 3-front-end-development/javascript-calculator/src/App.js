01f7f124eef7d907670db99bcb922155ae84e4c0
Lim En Xi <limenxi07@gmail.com>
2022-08-28 09:07:45 +0800

    event handler framework

--- 3-front-end-development/javascript-calculator/src/App.js
+++ 3-front-end-development/javascript-calculator/src/App.js
@@ -5,37 +5,43 @@ import { useState } from 'react';
 import './App.scss';
 
 const App = () => {
+  const [output, setOutput] = useState(0);
+
+  const handleNumber = (e) => {
+    setOutput(e.target.id);
+  };
+
   return (
     <div className="App">
       <Container id="main-container">
-        <p id="display">0</p>
+        <p id="display">{output}</p>
         <Row>
-          <Col className="button ac" id="clear" xs={6}>AC</Col>
-          <Col className="button ac" id="change-sign">+/-</Col>
-          <Col className="button operator" id="divide">÷</Col>
+          <Col onClick={handleClear} className="button ac" id="clear" xs={6}>AC</Col>
+          <Col onClick={handleNumber} className="button ac" id="change-sign">+/-</Col>
+          <Col onClick={handleOperator} className="button operator" id="divide">÷</Col>
         </Row>
         <Row>
-          <Col className="button" id="seven">7</Col>
-          <Col className="button" id="eight">8</Col>
-          <Col className="button" id="nine">9</Col>
-          <Col className="button operator" id="multiply">×</Col>
+          <Col onClick={handleNumber} className="button" id="7">7</Col>
+          <Col onClick={handleNumber} className="button" id="8">8</Col>
+          <Col onClick={handleNumber} className="button" id="9">9</Col>
+          <Col onClick={handleOperator} className="button operator" id="multiply">×</Col>
         </Row>
         <Row>
-          <Col className="button" id="four">4</Col>
-          <Col className="button" id="five">5</Col>
-          <Col className="button" id="six">6</Col>
-          <Col className="button operator" id="subtract">-</Col>
+          <Col onClick={handleNumber} className="button" id="4">4</Col>
+          <Col onClick={handleNumber} className="button" id="5">5</Col>
+          <Col onClick={handleNumber} className="button" id="6">6</Col>
+          <Col onClick={handleOperator} className="button operator" id="subtract">-</Col>
         </Row>
         <Row>
-          <Col className="button" id="one">1</Col>
-          <Col className="button" id="two">2</Col>
-          <Col className="button" id="three">3</Col>
-          <Col className="button operator" id="add">+</Col>
+          <Col onClick={handleNumber} className="button" id="1">1</Col>
+          <Col onClick={handleNumber} className="button" id="2">2</Col>
+          <Col onClick={handleNumber} className="button" id="3">3</Col>
+          <Col onClick={handleOperator} className="button operator" id="add">+</Col>
         </Row>
         <Row>
-          <Col className="button" id="zero" xs={6}>0</Col>
-          <Col className="button" id="decimal">.</Col>
-          <Col className="button operator" id="equals">=</Col>
+          <Col onClick={handleNumber} className="button" id="0" xs={6}>0</Col>
+          <Col onClick={handleNumber} className="button" id="decimal">.</Col>
+          <Col onClick={handleEvaluate} className="button operator" id="equals">=</Col>
         </Row>
       </Container>
     </div>
